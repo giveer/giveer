30e834c0e6c5118f955ab35512cf0789281482e1
giveer <giveer.in@gmail.com>
2023-11-21 20:33:31 +0530

    Initial setup commit-1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Comingsoon from './pages/coming soon/Comingsoon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Comingsoon />
   </React.StrictMode>
 );
 
