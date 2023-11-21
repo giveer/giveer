856216537e9099b54bb2b587ae255477dc849302
giveer <giveer.in@gmail.com>
2023-11-21 20:38:31 +0530

    Inital setup 2

---
+++ src/pages/coming soon/Comingsoon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Comingsoon.css';
+function Comingsoon() {
+    return (
+      <div className='coming_soon'>
+        <h1>GI<span>&#10084;</span>EER</h1>
+        <p>Coming Soon...</p>
+      </div>
+    );
+  }
+  
+  export default Comingsoon;
