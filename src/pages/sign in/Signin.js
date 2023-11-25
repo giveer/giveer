7b8e0909a689c30e020c922cc290ddfb9181d364
giveer <giveer.in@gmail.com>
2023-11-25 20:02:15 +0530

    signup validation

--- src/pages/sign in/Signin.js
+++ src/pages/sign in/Signin.js
@@ -3,7 +3,7 @@ function Signin() {
     return(
         <div className="center-div">
             <div>
-                <h4 className="g-h-4">Create Account</h4>
+                <h4 className="g-h-4">Login here</h4>
                 <p className="g-signup-p">Congratulations on taking the first step towards making a difference</p>
                 <div className="g-input-field-div-1">
                     <label className='g-lable-1'><i class="fa-regular fa-envelope g-input-icon-1"></i></label>
